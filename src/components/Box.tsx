2ce114dc4d6b955e2d6d9b57c23877824f66627d
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-24 15:33:00 -0300

    Using Styled Components

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,20 +1,17 @@
-import React from "react";
-
-interface StyleSheet {
-  fontFamily: string;
-}
+import { BaseComponent } from "@src/theme/BaseComponent";
+import { StyleSheet } from "@src/theme/StyleSheet";
+import React from 'react';
 
 interface BoxProps {
   tag: any;
   children: React.ReactNode;
   styleSheet: StyleSheet;
 }
-
 export default function Box({ styleSheet, children, tag }: BoxProps) {
   const Tag = tag || 'div';
   return (
-    <Tag style={styleSheet}>
+    <BaseComponent as={Tag} styleSheet={styleSheet}>
       {children}
-    </Tag>
+    </BaseComponent>
   );
 }
