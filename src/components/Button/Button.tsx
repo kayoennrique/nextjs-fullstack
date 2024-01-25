81fd800feef4c2982bf55c2c17e130f812bb83bd
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-25 13:13:50 -0300

    Creating base button component using ripple effect

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,11 @@
+import ButtonBase from "./ButtonBase";
+
+export default function Button() {
+  return (
+    <ButtonBase>
+      Button Master
+    </ButtonBase>
+  );
+}
+
+Button.Base = ButtonBase;
--- src/screens/HomeScreen/patterns/Feed/Feed.tsx
+++ src/screens/HomeScreen/patterns/Feed/Feed.tsx
@@ -4,6 +4,7 @@ import Text from "@src/components/Text/Text";
 import Icon from '@src/components/Icon/Icon';
 import Image from '@src/components/Image/Image';
 import Link from '@src/components/Link/Link';
+import Button from '@src/components/Button/Button';
 
 interface FeedProps {
   children: React.ReactNode;
@@ -22,15 +23,17 @@ export default function Feed({ children }) {
 Feed.Header = () => {
   return (
     <Box>
-      <Image
-        styleSheet={{
-          width: '128px',
-          height: '128px',
-          borderRadius: '100%',
-        }}
-        src="https://github.com/kayoennrique.png"
-        alt="Imagem de perfil do Kayo Ennrique"
-      />
+      <Button.Base href="https://github.com/kayoennrique">
+        <Image
+          styleSheet={{
+            width: '128px',
+            height: '128px',
+            borderRadius: '100%',
+          }}
+          src="https://github.com/kayoennrique.png"
+          alt="Imagem de perfil do Kayo Ennrique"
+        />
+      </Button.Base>
       <Link href="https://www.youtube.com/">
         <Icon name="youtube" />
       </Link>
