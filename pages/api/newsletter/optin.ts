b0c9b15d1a30f243a45f14db4d01d4b611002db8
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-26 18:36:47 -0300

    Introduction to Supabase

--- pages/api/newsletter/optin.ts
+++ pages/api/newsletter/optin.ts
@@ -1,4 +1,9 @@
+import { createClient } from "@supabase/supabase-js";
 import { NextApiRequest, NextApiResponse } from "next";
+
+const SUPABASE_URL = "https://zbnpfmfqljkvqvagscev.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpibnBmbWZxbGprdnF2YWdzY2V2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDYyOTc2NTAsImV4cCI6MjAyMTg3MzY1MH0.Wu7JtZ5PRCDJvjNbv3b7WKh3BJvXg01gs06WDlyGmNM";
+const dbClient = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 const httpStatus = {
   Success: 200,
@@ -8,13 +13,19 @@ const httpStatus = {
 };
 
 const controllerByMethod = {
-  POST(req: NextApiRequest, res: NextApiResponse) { // Cria coisas
+  async POST(req: NextApiRequest, res: NextApiResponse) { // Cria coisas
     console.log(req.body.emailNewsletter);
     res
       .status(httpStatus.Success)
       .json({ message: "Post request!" });
   },
-  GET(req: NextApiRequest, res: NextApiResponse) { // Retorna coisas
+  async GET(req: NextApiRequest, res: NextApiResponse) { // Retorna coisas
+    const { data, error } = await dbClient.from("newsletter_users")
+      .select("*")
+    console.log(data);
+    console.log(error);
+
+
     res
       .status(httpStatus.Success)
       .json({ message: "Get request!" });
